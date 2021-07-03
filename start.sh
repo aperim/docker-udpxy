24e0e964f9ac47275ae61ae8c2549940d0b47ff3
Troy Kelly <troy@troykelly.com>
2021-07-03 10:56:16 +1000

    Added settings

--- start.sh
+++ start.sh
@@ -12,8 +12,12 @@ if [ -z "${UDPXY_RENEW}" ]; then
     UDPXY_RENEW=60
 fi
 
+if [ -z "${UDPXY_CLIENTS}" ]; then
+    UDPXY_CLIENTS=1000
+fi
+
 echo Starting udpxy on port ${PORT}
 
-/usr/local/bin/udpxy -v -S -T -p ${PORT} -B ${UDPXY_BUFFER} -M ${UDPXY_RENEW}
+/usr/local/bin/udpxy -v -S -T -p ${PORT} -B ${UDPXY_BUFFER} -M ${UDPXY_RENEW} -c ${UDPXY_CLIENTS}
 
 echo Stopping udpxy on port ${PORT}
